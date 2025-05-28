9adea67036891d97fc3889df44f931802fdab430
wwmeynardie <109248825+wwmeynardie@users.noreply.github.com>
2025-05-28 16:34:23 -0400

    Fixed Title Bar on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,7 +46,7 @@
 			$(
 				'<div id="titleBar">' +
 					'<a href="#navPanel" class="toggle"></a>' +
-					'<span class="title">' + $('#logo').html() + '</span>' +
+					'<span class="title">' + 'William W. Meynardie' + '</span>' +
 				'</div>'
 			)
 				.appendTo($body);
